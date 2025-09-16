5a7a6f0510c8e76ab2ceec6b1c4017a8935805fa
almui <alarluci210604@gmail.com>
2025-09-15 21:12:08 -0300

    agrego titulos y más

--- Analisis.sql
+++ Analisis.sql
@@ -1,3 +1,4 @@
+-- Vista previa de las tablas principales del modelo de datos
 SELECT * FROM interaccion_ads LIMIT 10;
 SELECT * FROM ads;
 SELECT * FROM users;
@@ -6,7 +7,13 @@ SELECT * FROM ad_intereses;
 SELECT * FROM interes;
 SELECT * FROM user_intereses;
 
--- Cuántas impresiones, clics y compras tuvo cada anuncio.
+
+-- Cantidad total de cada tipo de interacción (clicks, compras, impresiones, etc.)
+SELECT tipo_interac, COUNT(interac_id) AS total_cant_interac
+FROM interaccion_ads
+GROUP BY tipo_interac;
+
+-- Total de impresiones, clics y compras por ad.
 
 SELECT ad_id, COUNT(interac_id) AS impresiones_por_ad
 FROM interaccion_ads
@@ -37,25 +44,13 @@ WITH compras as (
     WHERE i_a.tipo_interac="Purchase"
     GROUP BY i_a.ad_id    
 )
-SELECT i.ad_id, COUNT(i.interac_id) AS impresiones_por_ad, c.click_por_ad, c.compras_por_ad, c.click_por_ad/COUNT(i.interac_id) as cvr
+SELECT i.ad_id, COUNT(i.interac_id) AS impresiones_por_ad, c.click_por_ad, c.compras_por_ad
 FROM interaccion_ads i
 INNER JOIN compras c ON c.ad_id=i.ad_id
 WHERE i.tipo_interac="Impression"
 GROUP BY i.ad_id
 ORDER BY i.ad_id ASC;
-
-
-
--- Calcular métricas de marketing digital como CTR (_Click-Through Rate_) y CVR (_Conversion Rate_).
-SELECT tipo_interac, COUNT(interac_id) AS cant_interac
-FROM interaccion_ads
-WHERE ad_id=1
-GROUP BY tipo_interac;
-
-SELECT tipo_interac, COUNT(interac_id) AS total_cant_interac
-FROM interaccion_ads
-GROUP BY tipo_interac;
-
+-- Calcula el CTR (Click Through Rate) y CVR (Conversion Rate) por ad
 
 WITH compras as (
     WITH click as (
@@ -79,7 +74,7 @@ ORDER BY i.ad_id ASC;
 
 
 
--- Permite identificar qué anuncios fueron más efectivos dentro de una campaña, osea, qué anuncios tuvieron más compras
+-- Identifica los anuncios con más compras (mejor rendimiento)
 SELECT ad_id, COUNT(interac_id) AS compras_por_ad
 FROM interaccion_ads
 WHERE tipo_interac="Purchase"
@@ -88,14 +83,15 @@ ORDER BY compras_por_ad DESC;
 
 
 
--- Insights
--- Ad con las mas ventas segun el rango de edad 
+-- Anuncios con más compras por grupo de edad
 SELECT i_a.ad_id, COUNT(i_a.interac_id) AS compras_por_ad, u.grupo_edad
 FROM interaccion_ads as i_a
 LEFT JOIN users AS u ON i_a.user_id=u.id
 WHERE i_a.tipo_interac="Purchase"
 GROUP BY i_a.ad_id, u.grupo_edad
 ORDER BY compras_por_ad DESC;
+
+-- Compras por campaña y grupo etario del usuario
 
 SELECT c.nombre, COUNT(i_a.interac_id) AS compras_por_campania, u.grupo_edad
 FROM interaccion_ads as i_a 
@@ -106,6 +102,7 @@ WHERE i_a.tipo_interac="Purchase"
 GROUP BY c.nombre, u.grupo_edad
 ORDER BY compras_por_campania DESC;
 
+-- Total de compras por género, grupo de edad e intereses del usuario
 SELECT
     u.genero,
     u.grupo_edad,
@@ -120,6 +117,7 @@ WHERE i.tipo_interac = 'Purchase'
 GROUP BY u.genero, u.grupo_edad, interes.nombre
 ORDER BY total_compras DESC;
 
+-- Total de impresiones por género, edad e interés del usuario
 SELECT
     u.genero,
     u.grupo_edad,
@@ -132,7 +130,7 @@ WHERE i.tipo_interac = 'Impression'
 GROUP BY u.genero, u.grupo_edad, u.interes
 ORDER BY total_compras DESC;
 
-
+-- Total de compras según intereses del usuario y del anuncio
 
 SELECT
     COUNT(i.interac_id) AS total_compras,
@@ -146,7 +144,7 @@ GROUP BY u.interes, a.interes_target
 ORDER BY total_compras DESC;
 
 
--- ¿Qué campañas y anuncios tuvieron mejor rendimiento en términos de conversiones?
+-- ¿Qué campañas y anuncios tuvieron más conversiones (compras)?
 
 SELECT
     c.nombre AS nombre_campania,
@@ -163,7 +161,7 @@ ORDER BY total_compras DESC;
 
 
 
--- ¿Qué segmentos de usuarios son los más valiosos para la empresa?
+-- Identifica los segmentos (edad, género, ubicación) con más compras
 
 SELECT 
     u.pais,
@@ -178,8 +176,7 @@ GROUP BY u.pais, u.ciudad, u.grupo_edad, u.genero
 ORDER BY compras DESC;
 
 
--- ¿Cómo evoluciona el comportamiento de los usuarios a lo largo del tiempo?
-
+-- Evolución diaria de clics y compras
 SELECT 
     DATE(fecha) AS dia,
     COUNT(CASE WHEN tipo_interac = 'Click' THEN 1 END) AS total_clicks,
@@ -195,6 +192,8 @@ ORDER BY dia ASC;
 SELECT
     u.genero,
     u.grupo_edad,
+    u.ciudad,
+    u.pais,
     COUNT(i.interac_id) AS cantidad_de_usuarios,
     interes.nombre As intereses_user
 FROM interaccion_ads i
@@ -202,8 +201,12 @@ INNER JOIN users u ON i.user_id = u.id
 INNER JOIN ads a ON i.ad_id=a.id
 INNER JOIN user_intereses u_i ON i.user_id=u_i.user_id
 INNER JOIN interes ON interes.id=u_i.interes_id
-WHERE i.tipo_interac= "Click" AND i.tipo_interac != 'Purchase'
-GROUP BY u.genero, u.grupo_edad, interes.nombre
+WHERE i.tipo_interac= "Click" AND i.user_id NOT IN (
+    SELECT user_id
+    FROM interaccion_ads
+    WHERE tipo_interac = "Purchase"
+)
+GROUP BY u.genero, u.grupo_edad, interes.nombre, u.pais, u.ciudad 
 ORDER BY cantidad_de_usuarios DESC;
 -- ¿Quiénes comentan pero nunca dan like?
 SELECT
@@ -216,7 +219,11 @@ INNER JOIN users u ON i.user_id = u.id
 INNER JOIN ads a ON i.ad_id=a.id
 INNER JOIN user_intereses u_i ON i.user_id=u_i.user_id
 INNER JOIN interes ON interes.id=u_i.interes_id
-WHERE i.tipo_interac= "Comment" AND i.tipo_interac != 'Like'
+WHERE i.tipo_interac= "Comment" AND i.user_id NOT IN (
+    SELECT user_id
+    FROM interaccion_ads
+    WHERE tipo_interac = "Like"
+)
 GROUP BY u.genero, u.grupo_edad, interes.nombre
 ORDER BY cantidad_de_usuarios DESC;
 
@@ -225,12 +232,19 @@ SELECT
     u.genero,
     u.grupo_edad,
     COUNT(i.interac_id) AS cantidad_de_usuarios,
-    interes.nombre As intereses_user
+    interes.nombre As intereses_user,
+    SUM(CASE WHEN i.tipo_interac = 'Share' THEN 1 ELSE 0 END) AS cantidad_de_compratidos,
+    SUM(CASE WHEN i.tipo_interac = 'Purchase' THEN 1 ELSE 0 END) AS cantidad_de_compras
 FROM interaccion_ads i
 INNER JOIN users u ON i.user_id = u.id
 INNER JOIN ads a ON i.ad_id=a.id
 INNER JOIN user_intereses u_i ON i.user_id=u_i.user_id
 INNER JOIN interes ON interes.id=u_i.interes_id
-WHERE i.tipo_interac= "Comment" AND i.tipo_interac != 'Like'
 GROUP BY u.genero, u.grupo_edad, interes.nombre
+HAVING SUM(CASE WHEN i.tipo_interac = 'Share' THEN 1 ELSE 0 END) > 
+       SUM(CASE WHEN i.tipo_interac = 'Purchase' THEN 1 ELSE 0 END)
 ORDER BY cantidad_de_usuarios DESC;
+
+
+
+
